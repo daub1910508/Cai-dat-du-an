9588c30e14a8f690a7dafc8ed7d886790dd958c1
dau <daub1910508@student.ctu.edu.vn>
2022-09-20 21:20:27 +0700

    Cai dat express chay thong bao xin chao

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const config = require("./app/config");
+
+//start server
+const POST = config.app.port;
+app.listen(POST, () => {
+  console.log("Server is running on port ${PORT}");
+});
